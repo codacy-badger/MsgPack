70081c77bde7ec0e96b8bca30036631451e66853
Igor Tarasov <i.s.tarasov@gmail.com>
2017-07-11 13:32:22 +0300

    Add few simple unit tests.

---
+++ test/src/unit-package-integer.cpp
@@ -0,0 +1,56 @@
+#include "catch.hpp"
+#include <iostream>
+#include <msgpack/package.hpp>
+#include <msgpack/util.hpp>
+
+
+using namespace tarigo::msgpack;
+
+template <typename T, format FORMAT> void checkTypeAndFormat(int min, int max) {
+    for (int i = min; i <= max; i++) {
+        package p(static_cast<T>(i));
+        REQUIRE(p.type() == type::INTEGER);
+        REQUIRE(p.format() == FORMAT);
+    }
+}
+
+TEST_CASE("Integer - positive fixint", "[integer]") {
+    checkTypeAndFormat<int8_t, format::POSITIVE_FIXINT>(0, 127);
+    checkTypeAndFormat<int16_t, format::POSITIVE_FIXINT>(0, 127);
+    checkTypeAndFormat<int32_t, format::POSITIVE_FIXINT>(0, 127);
+    checkTypeAndFormat<int64_t, format::POSITIVE_FIXINT>(0, 127);
+
+    checkTypeAndFormat<uint8_t, format::POSITIVE_FIXINT>(0, 127);
+    checkTypeAndFormat<uint16_t, format::POSITIVE_FIXINT>(0, 127);
+    checkTypeAndFormat<uint32_t, format::POSITIVE_FIXINT>(0, 127);
+    checkTypeAndFormat<uint64_t, format::POSITIVE_FIXINT>(0, 127);
+}
+
+TEST_CASE("Integer - negative fixint", "[integer]") {
+    checkTypeAndFormat<int8_t, format::NEGATIVE_FIXINT>(-32, -1);
+    checkTypeAndFormat<int16_t, format::NEGATIVE_FIXINT>(-32, -1);
+    checkTypeAndFormat<int32_t, format::NEGATIVE_FIXINT>(-32, -1);
+    checkTypeAndFormat<int64_t, format::NEGATIVE_FIXINT>(-32, -1);
+}
+
+TEST_CASE("Integer - non fixints", "[integer]") {
+    package int8(static_cast<int8_t>(128));
+    package int16(static_cast<int16_t>(128));
+    package int32(static_cast<int32_t>(128));
+    package int64(static_cast<int64_t>(128));
+
+    REQUIRE(int8.format() == format::INT8);
+    REQUIRE(int16.format() == format::INT16);
+    REQUIRE(int32.format() == format::INT32);
+    REQUIRE(int64.format() == format::INT64);
+
+    package uint8(static_cast<uint8_t>(128));
+    package uint16(static_cast<uint16_t>(128));
+    package uint32(static_cast<uint32_t>(128));
+    package uint64(static_cast<uint64_t>(128));
+
+    REQUIRE(uint8.format() == format::UINT8);
+    REQUIRE(uint16.format() == format::UINT16);
+    REQUIRE(uint32.format() == format::UINT32);
+    REQUIRE(uint64.format() == format::UINT64);
+}
---
+++ test/src/unit-unpacker-incompete.cpp
@@ -0,0 +1,53 @@
+#include "catch.hpp"
+
+#include <iostream>
+#include <msgpack/msgpack.hpp>
+
+using namespace tarigo::msgpack;
+
+TEST_CASE("Incomplete fixstring", "[unpacker]") {
+    std::error_code ec;
+    std::vector<uint8_t> buf { 0xa3, 0x66, 0x6f };
+
+    auto pkg = unpacker::unpack(buf, ec);
+    REQUIRE(ec);
+    REQUIRE(ec == errc::unexpected_end_of_input);
+    REQUIRE(pkg.type() == type::NIL);
+    REQUIRE(pkg.format() == format::NIL);
+}
+
+TEST_CASE("Incomplete fixstring - format byte only", "[unpacker]") {
+    std::error_code ec;
+    std::vector<uint8_t> buf { 0xa3 };
+
+    auto pkg = unpacker::unpack(buf, ec);
+    REQUIRE(ec);
+    REQUIRE(ec == errc::unexpected_end_of_input);
+    REQUIRE(pkg.type() == type::NIL);
+    REQUIRE(pkg.format() == format::NIL);
+}
+
+
+TEST_CASE("Incomplete str 8 - format byte only", "[unpacker]") {
+    std::error_code ec;
+    std::vector<uint8_t> buf { 0xd9, 0x20, 0x66, 0x6f, 0x6f, 0x6f, 0x6f, 0x6f,
+                               0x6f, 0x6f, 0x6f, 0x6f, 0x6f, 0x6f, 0x6f, 0x6f,
+                               0x6f, 0x6f, 0x6f, 0x6f, 0x6f, 0x6f, 0x6f, 0x6f,
+                               0x6f, 0x6f, 0x6f, 0x6f, 0x6f, 0x6f, 0x6f, 0x6f,
+                               0x6f, };
+    auto pkg = unpacker::unpack(buf, ec);
+    REQUIRE(ec);
+    REQUIRE(ec == errc::unexpected_end_of_input);
+    REQUIRE(pkg.type() == type::NIL);
+    REQUIRE(pkg.format() == format::NIL);
+}
+
+TEST_CASE("Incomplete str 8", "[unpacker]") {
+    std::error_code ec;
+    std::vector<uint8_t> buf { 0xd9 };
+    auto pkg = unpacker::unpack(buf, ec);
+    REQUIRE(ec);
+    REQUIRE(ec == errc::unexpected_end_of_input);
+    REQUIRE(pkg.type() == type::NIL);
+    REQUIRE(pkg.format() == format::NIL);
+}

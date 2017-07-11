90088d497b35222c578ce14c107c9c822e3e7c7f
Igor Tarasov <i.s.tarasov@gmail.com>
2017-07-11 19:30:17 +0300

    Unit tests: pack/unpack fixed string.

--- test/src/unit-pack-unpack.cpp
+++ test/src/unit-pack-unpack.cpp
@@ -1,14 +1,18 @@
 #include <catch.hpp>
-#include <vector>
-#include <system_error>
-
 #include <msgpack/msgpack.hpp>
 
+#include <vector>
+#include <map>
+#include <system_error>
+
 namespace msgpack = tarigo::msgpack;
+
 using std::vector;
+using std::map;
+using std::string;
 using std::error_code;
 
-TEST_CASE("Pack/unpack nil", "[pack/unpack]") {
+TEST_CASE("Check nil value", "[pack/unpack]") {
     msgpack::package nil, nil_explicit(nullptr);
 
     REQUIRE(nil == nil_explicit);
@@ -27,7 +31,7 @@ TEST_CASE("Pack/unpack nil", "[pack/unpack]") {
     REQUIRE(nil_explicit == unpacked);
 }
 
-TEST_CASE("Pack/unpack boolean", "[pack/unpack]") {
+TEST_CASE("Check boolean values", "[pack/unpack]") {
     msgpack::package f(false), t(true);
 
     REQUIRE(f.type() == msgpack::type::BOOLEAN);
@@ -58,4 +62,45 @@ TEST_CASE("Pack/unpack boolean", "[pack/unpack]") {
     auto unpacked_t = msgpack::unpacker::unpack(raw_msgpack, ec);
     REQUIRE(!ec);
     REQUIRE(t == unpacked_t);
+}
+
+TEST_CASE("Check fixed string", "[pack/unpack]") {
+    map<string, vector<uint8_t>> sample {
+        { "", {0xa0} },
+        { "f", {0xa1, 0x66} },
+        { "foo", {0xa3, 0x66, 0x6f, 0x6f} },
+        { "fooo",  {0xa4, 0x66, 0x6f, 0x6f, 0x6f} },
+        { "foooo", {0xa5, 0x66, 0x6f, 0x6f, 0x6f, 0x6f} },
+        { "fooooo", {0xa6, 0x66, 0x6f, 0x6f, 0x6f, 0x6f, 0x6f} }
+        //TODO: ...
+    };
+
+    for (auto entry : sample) {
+        msgpack::package package(entry.first);
+
+        REQUIRE(package.type() == msgpack::type::RAW_STRING);
+        REQUIRE(package.format() == msgpack::format::FIXSTR);
+
+        vector<uint8_t > raw_msgpack;
+        package.pack(raw_msgpack);
+
+        REQUIRE(entry.second == raw_msgpack);
+    }
+
+    //TODO: Range tests for fixed values.
+    map<string, vector<uint8_t>> one_byte_more_than_fixstr {
+        {
+            "this_is_not_a_fixed_string_32byt",
+            {0xbe, 0x74, 0x68, 0x69, 0x73, 0x5f, 0x69, 0x73, 0x5f, 0x6e, 0x6f,
+             0x74, 0x5f, 0x61, 0x5f, 0x66, 0x69, 0x78, 0x65, 0x64, 0x5f, 0x73,
+             0x74, 0x72, 0x69, 0x6e, 0x67, 0x5f, 0x33, 0x32, 0x62}
+        }
+    };
+
+    for(auto entry : one_byte_more_than_fixstr) {
+        msgpack::package package(entry.first);
+        REQUIRE(package.type() == msgpack::type::RAW_STRING);
+        REQUIRE(package.format() != msgpack::format::FIXSTR);
+        REQUIRE(package.format() == msgpack::format::STR8);
+    }
 }
